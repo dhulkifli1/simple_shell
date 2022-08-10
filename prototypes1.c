95e2610e102ec4bcecd908cbb3ea72cf400380d9
root <Dhulkifli>
2022-08-10 07:28:04 -0700

    prototypes 1 and 2

--- prototypes1.c
+++ prototypes1.c
@@ -2,23 +2,99 @@
 
 int split_line(cmd_data *data)
 {
-return(0);
+	char *token;
+	size_t size = TOKENSIZE, new_size, i = 0;
+
+	if (_strcmp(data->line, "\n") == 0)
+		return (FAIL);
+	data->args = malloc(size * sizeof(char *));
+	if (data->args == NULL)
+		return (FAIL);
+	token = strtok(data->line, DELIMITER);
+	if (token == NULL)
+		return (FAIL);
+	while (token)
+	{
+		data->args[i++] =  token;
+		if (i + 2 >= size)
+		{
+			new_size = size * 2;
+			data->args = _realloc(data->args, size * sizeof(char *),
+					new_size * sizeof(char *));
+			if (data->args == NULL)
+				return (FAIL);
+			size = new_size;
+		}
+		token = strtok(NULL, DELIMITER);
+	}
+	data->args[i] = NULL;
+	return (0);
 }
+
+
 
 int free_data(cmd_data *data)
 {
-return(0);
+	free(data->line);
+	data->line = NULL;
+	free(data->args);
+	data->args = NULL;
+	free(data->cmd);
+	data->cmd = NULL;
+	free(data->error_msg);
+	data->error_msg = NULL;
+	return (0);
 }
+
+
 
 int parse_line(cmd_data *data)
 {
-return(0);
+	if (is_path_form(data) > 0)
+		return (SUCCESS);
+	if (is_builtin(data) > 0)
+	{
+		if (handle_builtin(data) < 0)
+			return (FAIL);
+		return (NEUTRAL);
+	}
+	is_short_form(data);
+	return (SUCCESS);
 }
+
+
+
 char *_strcat(char *first, char *second)
 {
+	int len1, len2, i = 0, j = 0;
+	char *result;
 
+	len1 = _strlen(first);
+	len2 = _strlen(second);
+	result = malloc((len1 + len2 + 2) * sizeof(char));
+	if (!result)
+		return (NULL);
+	*result = '\0';
+	while (first[j])
+		result[i++] = first[j++];
+	result[i++] = '/';
+	j = 0;
+	while (second[j])
+		result[i++] = second[j++];
+	result[i] = '\0';
+	return (result);
 }
+
+
+
 char *_strchr(char *str, char c)
 {
+	char *ptr;
 
+	if (str == NULL)
+		return (NULL);
+	for (ptr = str; *ptr; ptr++)
+		if (*ptr == c)
+			return (ptr);
+	return (NULL);
 }
--- prototypes2.c
+++ prototypes2.c
@@ -1,21 +1,87 @@
 #include "main.h"
 int _strcmp(char *s1, char *s2)
 {
+	int cmp = 0, i;
 
+	if (s1 == NULL || s2 == NULL)
+		return (1);
+	for (i = 0; s1[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			cmp = s1[i] - s2[i];
+			break;
+		}
+		else
+			continue;
+	}
+	return (cmp);
 }
+
+
+
 char *_getenv(char *path_name)
 {
+	char **environ_cursor, *env_ptr, *name_ptr;
 
+	environ_cursor = environ;
+	while (*environ_cursor)
+	{
+		env_ptr = *environ_cursor;
+		name_ptr = path_name;
+		while (*env_ptr == *name_ptr)
+		{
+			if (*env_ptr == '=')
+				break;
+			env_ptr++;
+			name_ptr++;
+		}
+		if ((*env_ptr == '=') && (*name_ptr == '\0'))
+			return (env_ptr + 1);
+		environ_cursor++;
+	}
+	return (NULL);
 }
+
+
+
 void index_cmd(cmd_data *data)
 {
-
+	data->index += 1;
 }
+
+
+
 int write_history(cmd_data *data)
 {
+	char *filename = "history";
+	char *line_of_history = "this is a line of history";
+	ssize_t fd, w;
+	int len;
 
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if (line_of_history)
+	{
+		while (line_of_history[len])
+			len++;
+		w = write(fd, line_of_history, len);
+		if (w < 0)
+			return (-1);
+	}
+	return (1);
 }
+
+
+
 int _isalpha(int c)
 {
-
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (SUCCESS);
+	}
+	return (FAIL);
 }
